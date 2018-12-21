f00b5dce1cce6c77eb7647cdb02b1ec36b244d15
Ben Taylor <ben-j.taylor@eon.com>
2018-12-21 12:53:16 +0000

    initial commit

---
+++ RegexHelper/MainModel.cs
@@ -0,0 +1,46 @@
+﻿namespace RegexHelper
+{
+    using System.Text.RegularExpressions;
+    using System.Windows;
+
+    public class MainModel : DependencyObject
+    {
+        // Using a DependencyProperty as the backing store for BaseString.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BaseStringProperty =
+            DependencyProperty.Register("BaseString", typeof(string), typeof(MainModel), new PropertyMetadata(string.Empty));
+
+        // Using a DependencyProperty as the backing store for RegexString.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RegexStringProperty =
+            DependencyProperty.Register("RegexString", typeof(string), typeof(MainModel), new PropertyMetadata(string.Empty));
+
+        public string BaseString
+        {
+            get => (string)this.GetValue(BaseStringProperty);
+            set => this.SetValue(BaseStringProperty, value);
+        }
+
+        public string RegexString
+        {
+            get => (string)this.GetValue(RegexStringProperty);
+            set => this.SetValue(RegexStringProperty, value);
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            if (e.Property == BaseStringProperty)
+            {
+                var escapeChars = @"\.$^{[(|)*+?".ToCharArray();
+                var newRegexString = (string)e.NewValue;
+                foreach (var escapeChar in escapeChars)
+                {
+                    newRegexString = newRegexString.Replace($"{escapeChar}", $@"\{escapeChar}");
+                }
+
+                newRegexString = Regex.Replace(newRegexString, @"\s+", @"[\s\n]*");
+
+                this.RegexString = newRegexString;
+            }
+        }
+    }
+}
---
+++ RegexHelper/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace RegexHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainModel();
+        }
+    }
+}
